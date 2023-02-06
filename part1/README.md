b352cebbc53d3541d247174fe60f59f67740b21b
sajs1 <sneha.saj@aalto.fi>
2023-02-06 12:46:47 +0200

    step 2

--- part1/README.md
+++ part1/README.md
@@ -1,3 +1,3 @@
 courseinfo/mystack => Exercises 1.1-1.2, Exercises 1.3-1.5 
-unicafe =>  Exercises 1.6-1.10
-anecdotes => Exercises 1.11-1.14
+unicafe =>  Exercises 1.6-1.11
+anecdotes => Exercises 1.12-1.14
--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,15 +13,24 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0});
+
   const randomAnecdote = () => {
     var randomNumber = Math.floor(Math.random()*anecdotes.length);
     setSelected(randomNumber);
+  }
+  const vote = () => {
+    const copy = { ...votes};
+    copy[selected]+=1; 
+    setVotes(copy);
   }
   
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={vote}>vote</button>
       <button onClick={randomAnecdote}>next anecdote</button>
     </div>
   )
