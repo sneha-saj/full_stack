f56b2e6b3d60f80affa42b642a6eb41df843cc11
sajs1 <sneha.saj@aalto.fi>
2023-02-06 12:56:34 +0200

    step 3

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -15,6 +15,8 @@ const App = () => {
    
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0});
+  const [mostVoted, setMostVoted] = useState(0);
+  const [voteCount, setvoteCount] = useState(0);
 
   const randomAnecdote = () => {
     var randomNumber = Math.floor(Math.random()*anecdotes.length);
@@ -23,15 +25,29 @@ const App = () => {
   const vote = () => {
     const copy = { ...votes};
     copy[selected]+=1; 
+    console.log("selected=",selected);
+    console.log("copy=",copy[selected]);
+    console.log(mostVoted);
+    if(copy[selected] > voteCount)
+    {
+      const newMostVoted = selected;
+      const newVoteCount = copy[selected];
+      setvoteCount(newVoteCount);
+      setMostVoted(newMostVoted);
+    }
     setVotes(copy);
   }
   
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={vote}>vote</button>
       <button onClick={randomAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVoted]}</p>
+      <p>has {votes[mostVoted]} votes</p>
     </div>
   )
 }
