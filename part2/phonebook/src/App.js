984c6d898e23b9be5a4af27c776f25986e952ab5
sajs1 <sneha.saj@aalto.fi>
2023-07-18 12:18:33 +0300

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,16 +4,16 @@ import PersonForm from './Components/PersonForm.js';
 import Filter from './Components/Filter.js'; 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/persons')
+      .then(response => response.json())
+      .then(data => setPersons(data));
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
