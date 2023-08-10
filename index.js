7b9757e8704b414e7cedaeb3f7b0bd7c6e2196f0
sajs1 <sneha.saj@aalto.fi>
2023-08-10 11:08:27 +0300

    3.2: phonebook backend step2

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const express = require('express');
+const express = require("express");
+const moment = require("moment-timezone");
 const app = express();
 const PORT = 3001;
 
 const persons = [
-  { id: 1, name: 'Arto Hellas', phone: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', phone: '99-44-5323523'},
-  { id: 3, name: 'Dan Abramov', phone: '12-43-234345'},
-  { id: 4, name: 'Mary Poppendick', phone: '39-23-6423122'},
+  { id: 1, name: "Arto Hellas", phone: "040-123456" },
+  { id: 2, name: "Ada Lovelace", phone: "99-44-5323523" },
+  { id: 3, name: "Dan Abramov", phone: "12-43-234345" },
+  { id: 4, name: "Mary Poppendick", phone: "39-23-6423122" },
 ];
 
-app.get('/api/persons', (req, res) => {
+app.get("/api/persons", (req, res) => {
   res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const currentTime = moment()
+    .tz("Europe")
+    .format("ddd MMM DD YYYY HH:mm:ss ZZ");
+  const numPersons = persons.length;
+  res.send(`
+      <div>
+        <p>Phonebook has info for ${numPersons} people</p>
+        <p>${currentTime}</p>
+      </div>
+    `);
 });
 
 app.listen(PORT, () => {
